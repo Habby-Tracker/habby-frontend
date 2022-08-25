4a5973c1710846ba15d3656da82c380586db4b19
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-25 14:57:11 -0700

    add mood service

---
+++ src/State/Services/mood-service.js
@@ -0,0 +1,63 @@
+export async function insertMood(mood) {
+    const rawResponse = await fetch(`${process.env.API_URL}/moods`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        mode: 'cors',
+        body: JSON.stringify(mood),
+    });
+    const data = await rawResponse.json();
+
+    return data;
+}
+
+export async function getMoodById(id) {
+    const rawResponse = await fetch(`${process.env.API_URL}/moods/${id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        mode: 'cors',
+    });
+    const data = await rawResponse.json();
+
+    return data;
+}
+
+export async function getAllMoodsByUser() {
+    const rawResponse = await fetch(`${process.env.API_URL}/moods`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        mode: 'cors',
+    });
+    const data = await rawResponse.json();
+
+    return data;
+}
+
+export async function updateMood(id, updatedMood) {
+    const rawResponse = await fetch(`${process.env.API_URL}/moods/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        mode: 'cors',
+        body: JSON.stringify(updatedMood),
+    });
+
+    const data = await rawResponse.json();
+
+    return data;
+}
+
+export async function deleteMood(id) {
+    const rawResponse = await fetch(`${process.env.API_URL}/moods/${id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        mode: 'cors',
+    });
+
+    const data = await rawResponse.json();
+
+    return data;
+}
