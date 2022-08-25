7fb091ff09876e2b6b4f4f3f99895f8f6d54b875
Will Gundy <willgunderson503@gmail.com>
2022-08-25 14:25:27 -0700

    got tailwind working on main project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import Routes from './components/Routes.jsx';
+// import { Toaster } from 'react-hot-toast';
+// import UserProvider from './State/Context/UserContext.jsx';
+import './App.css';
+
+
+export default function App() {
+    return <h1 className="xl:text-pink-500 text-4xl font-bold newCSS">Hello world! I am using React</h1>;
+}
