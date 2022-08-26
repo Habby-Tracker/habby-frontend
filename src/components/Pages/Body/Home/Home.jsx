894a611f7e6df5e054c265acd4938e4cb458aace
SusanBird <susanbird391@gmail.com>
2022-08-26 12:37:56 -0700

    goal card created

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Button from '../Global/Button/Button';
 import HabitCard from '../Global/Card/HabitCard';
+import GoalCard from '../Global/Card/GoalCard';
 import Section from '../Global/Section/Section';
 
 export default function Home() {
@@ -9,6 +10,9 @@ export default function Home() {
             <Section header="Today's Habits" seeAll={true} width="90vw">
                 <HabitCard text="Go to the gym" completed={false} width="100%" height="100px" id={1}/>
                 <Button text="Button" width={'80%'}/>
+            </Section>
+            <Section header = "Your Goals">
+                <GoalCard goal="50 push ups" target="5 of 7 days target" freq="Everyday" width="100%" height="150px" id={1}/>
             </Section>
         </div>
     );
