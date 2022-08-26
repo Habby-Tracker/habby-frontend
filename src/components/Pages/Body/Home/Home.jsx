498818caa0e4aac66dd14e9ec1e85c27a0359075
Will Gundy <willgunderson503@gmail.com>
2022-08-26 10:25:49 -0700

    global section containers

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Button from '../Global/Button/Button';
+import Section from '../Global/Section/Section';
 
 export default function Home() {
     return (
         <div>
             <p>Home Page</p>
-            <Button text="Button" width={'100%'}/>
+            <Section header="Today's Habits" seeAll={true} width="90vw">
+                <Button text="Button" width={'80%'}/>
+            </Section>
         </div>
     );
 }
