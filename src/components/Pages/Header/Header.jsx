3aef0044514e66803fa5d429af96a2598860f197
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-26 09:37:29 -0700

    intial setup for Header component

--- src/components/Pages/Header/Header.jsx
+++ src/components/Pages/Header/Header.jsx
@@ -1,27 +1,35 @@
-import Menu from './Menu';
-import Navigation from './Navigation';
-import User from './User';
+// import { useContext } from 'react';
+// import { UserStateContext } from '../../../State/Context/UserContext';
+// import Menu from './Menu';
+// import Navigation from './Navigation';
+// import User from './User';
 
-const nav = [
-    { to: '/', label: 'Home' },
-    { to: 'about', label: 'About' },
-    { to: 'progress', label: 'Progress' },
-];
+// const nav = [
+//     { to: '/', label: 'Home' },
+//     { to: 'about', label: 'About' },
+//     { to: 'progress', label: 'Progress' },
+// ];
 
 export default function Header() {
+    // const user = useContext(UserStateContext);
+    const user = {
+        firstName: 'Riley',
+        lastName: 'Hoffman',
+        email: 'rileyjhoff@gmail.com',
+        avatar: ''
+    };
+
     return (
-        <header>
-            <div>
-                <Menu />
+        <header className="flex flex-row justify-between align-center bg-white">
+            <div className="px-4 py-1 text-lg">
+                {'Hello, '}
+                <span className="text-primaryOrange">
+                    {user.firstName}!
+                </span>
             </div>
-
-            <h1>My React App</h1>
-
-            <div>
-                <Navigation navigation={nav} />
+            <div className="px-4 py-1">
+                Profile Nav Stuff
             </div>
-
-            <User username={'Will'}/>
         </header>
     );
 }
