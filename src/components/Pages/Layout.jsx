901fdc65e2f5f9631b0051211c3aca553310a86b
Beau Elliott <belliott1516@gmail.com>
2022-08-26 14:30:04 -0700

    adding pie chart compoenent

--- src/components/Pages/Layout.jsx
+++ src/components/Pages/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import PieChart from './Body/Global/PieChart/PieChart.jsx';
 import Header from './Header/Header.jsx';
 import Navigation from './Header/Navigation.jsx';
 
@@ -20,9 +21,10 @@ export default function Layout() {
             <main className="flex justify-center bg-white">
                 <Outlet />
             </main>
-            <footer className="container flex justify-center bg-white min-w-fit md:hidden">
+            <footer className="container flex justify-center bg-white w-full md:hidden">
                 <div className="flex justify-between space-x-0 md:space-x-6">
                     <Navigation navigation={nav} />
+                    <PieChart progress={50}/>
                 </div>
             </footer>
         </div>
