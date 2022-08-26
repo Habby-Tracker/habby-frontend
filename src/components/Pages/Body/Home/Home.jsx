6d02db6df5257c7a65c0bb31d3ac5121e9a423f3
SusanBird <susanbird391@gmail.com>
2022-08-26 12:42:53 -0700

    layout of sections

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -11,7 +11,8 @@ export default function Home() {
                 <HabitCard text="Go to the gym" completed={false} width="100%" height="100px" id={1}/>
                 <Button text="Button" width={'80%'}/>
             </Section>
-            <Section header = "Your Goals">
+            <br></br>
+            <Section header = "Your Goals" seeAll={true} width="90vw">
                 <GoalCard goal="50 push ups" target="5 of 7 days target" freq="Everyday" width="100%" height="150px" id={1}/>
             </Section>
         </div>
