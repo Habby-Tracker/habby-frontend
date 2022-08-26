af4d3302df9d0073f4062b67a4b8263b5ab7c7d0
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-26 12:44:39 -0700

    refactor Dates to map to DateCard for each date within the 5 day range

--- src/components/Pages/Header/Dates.jsx
+++ src/components/Pages/Header/Dates.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
+import DateCard from './DateCard';
 
 export default function Dates() {
     const [selectedDate, setSelectedDate] = useState(new Date());
@@ -13,38 +14,15 @@ export default function Dates() {
     const nextDate2 = new Date(selectedDate);
     nextDate2.setDate(selectedDate.getDate() + 2);
 
-    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-    function getShortMonthName(monthIndex) {
-        return monthNames[monthIndex];
-    }
     useEffect(() => {
         setDates([]);
         setDates([prevDate2, prevDate1, selectedDate, nextDate1, nextDate2]);
     }, [selectedDate]);
-    
-    function handleChangeDate(date) {
-        setSelectedDate(new Date(date));
-    }
-    return (<div autoFocus={true} className="flex flex-row justify-center align-center bg-white">
-        {dates.map((date, index) => {
-            if (index === 2) { 
-                return (
-                    <div key={index} className="flex flex-col pt-2 pb-4 px-4 m-1 bg-primaryOrange/[.15] border border-primaryOrange/[.15] rounded select-none hover:cursor-default">
-                        <span className="text-primaryOrange text-lg font-bold">{date.getDate()}</span>
-                        <span className="text-primaryOrange text-sm font-light leading-3">{getShortMonthName(date.getMonth())}</span>
-                    </div>
-                );
-            } else {
-                return (
-                    <div key={index} className="flex flex-col pt-2 pb-4 px-4 m-1 bg-white border border-gray-200 rounded select-none sm:hover:bg-primaryOrange/[.15] sm:hover:border-primaryOrange/[.15] hover:cursor-pointer" onClick={() => handleChangeDate(date)}>
-                        <span className="text-lg font-semibold">{date.getDate()}</span>
-                        <span className="text-sm font-light leading-3">{getShortMonthName(date.getMonth())}</span>
-                    </div>
-                );
+
+    return (
+        <div className="flex flex-row justify-center align-center bg-white">
+            {dates.map((date, index) => <DateCard key={index} date={date} index={index} setSelectedDate={setSelectedDate} />)
             }
-        })
-        }
-    </div>
+        </div>
     );
 }
