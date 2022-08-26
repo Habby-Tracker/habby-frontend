a791929556b987fa7b862b6f26e785b39237f5b0
Beau Elliott <belliott1516@gmail.com>
2022-08-26 16:55:26 -0700

    small changes

--- src/components/Pages/Body/Global/ProgressBar/ProgressBar.jsx
+++ src/components/Pages/Body/Global/ProgressBar/ProgressBar.jsx
@@ -7,7 +7,7 @@ export default function ProgressBar({ progress }) {
 
     return (
         <div>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '15px' }}>
                 <Box sx={{ width: '85%', mr: 1 }}>
                     <LinearProgress variant="determinate" value={progress} color="secondary" sx={{ height: '10px', borderRadius: '10px' }} />
                 </Box>
