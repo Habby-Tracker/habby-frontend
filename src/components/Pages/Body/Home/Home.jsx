cb83286f1c242d127d2ba9d64a345e1119b9f8d3
Will Gundy <willgunderson503@gmail.com>
2022-08-29 12:15:48 -0700

    created the create goal button

--- src/components/Pages/Body/Home/Home.jsx
+++ src/components/Pages/Body/Home/Home.jsx
@@ -3,11 +3,13 @@ import HabitCard from '../Global/Card/HabitCard';
 import GoalCard from '../Global/Card/GoalCard';
 import Section from '../Global/Section/Section';
 import GoalAdded from '../GoalAdded/GoalAdded';
+import AddGoalButton from '../Global/Button/AddGoalButton';
 
 export default function Home() {
     return (
         <div>
             <p>Home Page</p>
+            <AddGoalButton />
             <Section header="Today's Habits" seeAll={true} width="90vw">
                 <HabitCard text="Go to the gym" completed={false} width="100%" height="100px" id={1}/>
                 <Button text="Button" width={'80%'}/>
