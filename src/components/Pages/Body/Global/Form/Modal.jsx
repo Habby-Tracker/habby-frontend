7bda2a603cddcc477cd601b202a4dfb02d77f993
Cody-Wise <cody@sharptackmedia.com>
2022-08-29 11:41:31 -0700

    Updated modal to use children

--- src/components/Pages/Body/Global/Form/Modal.jsx
+++ src/components/Pages/Body/Global/Form/Modal.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import Button from '../Button/Button';
-import Form from './Form';
+// import Form from './Form';
 
-const Modal = () => {
+const Modal = ({ children }) => {
     const [showModal, setShowModal] = useState(false);
     return (
         <>
@@ -13,7 +13,7 @@ const Modal = () => {
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                                    <Form />
+                                    {children}
                                 </div>
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                                     <Button
