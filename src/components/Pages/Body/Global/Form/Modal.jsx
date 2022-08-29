b3cebc7eeec6181d4d54911776104346630bcd88
Cody-Wise <cody@sharptackmedia.com>
2022-08-29 10:03:14 -0700

    Added bg blur to modal

--- src/components/Pages/Body/Global/Form/Modal.jsx
+++ src/components/Pages/Body/Global/Form/Modal.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import Button from './Button';
+import Button from '../Button/Button';
 import Form from './Form';
 
 const Modal = () => {
     const [showModal, setShowModal] = useState(false);
     return (
         <>
-            <Button />
+            <Button onClick={() => setShowModal(true)} />
             {showModal ? (
                 <>
-                    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="backdrop-blur-md bg-white/30 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
