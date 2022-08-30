239be13f416cab8298c5c8c6ce412645c6432c16
Beau Elliott <belliott1516@gmail.com>
2022-08-30 16:38:14 -0700

    adding in analytics page and building out styling

--- src/components/Pages/Body/Analytics/Analytics.jsx
+++ src/components/Pages/Body/Analytics/Analytics.jsx
@@ -1,17 +1,44 @@
+import { useGoals } from '../../../../State/Hooks/goals';
+import PieChart from '../Global/PieChart/PieChart';
 import Section from '../Global/Section/Section';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import ProgressCard from '../Global/Card/ProgressCard';
 
 export default function List() {
+    const { goals } = useGoals();
+    const complete = 50;
+    const goal = 60;
+    const progress = (complete / goal) * 100;
 
     return (
-        <>
+        <div className="flex flex-col min-w-full">
             <Section
-                header="Progress"
+                header="Analytics"
                 subText="A section to detail the users progress"  
-                maxWidth="90%">
+                maxWidth="100%">
+                <div className="container flex justify-center items-center flex-col mx-auto">
+                    <PieChart
+                        progress={progress}
+                        size={'150px'}
+                        ringcolor={'hsla(21.6, 100%, 50%, 1)'}
+                        textcolor={'hsla(315, 97%, 26%, 1)'}
+                        textvariant={'h3'}
+                    />
+                    <p className="text-primaryOrange text-sm text-bold flex">
+                        <CheckIcon /> {`${complete} Habits have been completed!`}
+                    </p>
+                    <p className="text-primaryBlack text-sm text-bold flex">
+                        <CloseIcon /> {`You are still working on ${goal - complete} Habits!`}
+                    </p>
+                </div>
+                
             </Section>
-            <Section  
-                maxWidth="90%" >
+            <Section
+                header="Current Goals"  
+                maxWidth="100%" >
+                {goals && goals.map((goal) => <ProgressCard key={`goal card${goal.id}`} goal={goal.goalName} target={50} />)}
             </Section>
-        </>
+        </div>
     );
 }
--- src/components/Pages/Body/Global/Card/ProgressCard.jsx
+++ src/components/Pages/Body/Global/Card/ProgressCard.jsx
@@ -14,15 +14,15 @@ export default function ProgressCard({ goal, target, width, height, id }) {
         <div className="flex flex-row w-full items-center rounded-md bg-lightGrey shadow-md"
             style={{ width, height }} value={id}>
             <div className="w-1/5">
-                <CircularProgressWithLabel progress={progress} />
+                <CircularProgressWithLabel progress={progress} textvariant={'caption'} />
             </div>    
             <div className="flex flex-row w-3/5 items-center">
                 <div className="w-full items-left">
                     <p className={goalText}>{goal}</p>
-                    <p className={targetDays}>{target}</p>
+                    <p className={targetDays}>{target} days</p>
                 </div>
             </div>
-            <div className="w-1/5 justify-center"> 
+            <div className="w-1/5 justify-center px-2"> 
                 <StatusPill width={'100%'} />            
             </div>
         </div>
