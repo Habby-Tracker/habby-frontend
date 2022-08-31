486269b244f7cd900a5bee30c57ed4a73fa5ec61
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-31 14:17:54 -0700

    inital setup to link GoalStats to context

--- src/components/Pages/Body/GoalStats/GoalStats.jsx
+++ src/components/Pages/Body/GoalStats/GoalStats.jsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { useGoals } from '../../../../State/Hooks/goals';
+import { useParams } from 'react-router-dom';
+import { useGoals } from '../../../../State/Hooks/goals';
+import { useHabits } from '../../../../State/Hooks/habits';
 // import useLookups from '../../../../State/Hooks/lookups';
 import Section from '../Global/Section/Section';
 
@@ -27,28 +28,32 @@ export default function GoalStats() {
     //     daysFailed: 1
     // });
 
-    const goal = {
-        goalCategoryID: '1',
-        categoryName: 'Mental Health',
-        goalName: 'Journal everyday',
-        timePeriodID: '1',
-        dayCount: 7,
-        habitTypeID: '1',
-        habitType: 'Daily',
-        habitName: 'Journaling',
-        statusID: '1',
-        statusName: 'Active',
-        created_at: '2022-08-29',
-        daysCompleted: 5,
-        daysFailed: 1
-    };
-    const createdAtDate = new Date(goal.created_at);
-    // const { goals } = useGoals();
+    // const goal = {
+    //     goalCategoryID: '1',
+    //     categoryName: 'Mental Health',
+    //     goalName: 'Journal everyday',
+    //     timePeriodID: '1',
+    //     dayCount: 7,
+    //     habitTypeID: '1',
+    //     habitType: 'Daily',
+    //     habitName: 'Journaling',
+    //     statusID: '1',
+    //     statusName: 'Active',
+    //     created_at: '2022-08-29',
+    //     daysCompleted: 5,
+    //     daysFailed: 1
+    // };
     // const { status, timePeriod, habitType } = useLookups();
     // // // need to get all habits by goal id
     // const { habits } = useHabits();
-    // const { id } = useParams();
-    // const thisGoal = goals.filter(goal => goal.id === id);
+    const { id } = useParams();
+    const { goals } = useGoals();
+    const { habits } = useHabits();
+    const thisGoal = goals.filter(goal => goal.id === id);
+    const goalHabits = (habits && habits.length) ? habits.filter(habit => habit.goalID === Number(thisGoal.id)) : null;
+    const totalHabits = goalHabits && goalHabits.length;
+    const completedHabits = goalHabits && goalHabits.filter(habit => habit.statusID === '3').length;
+    const createdAtDate = new Date(thisGoal.createdAt).toDateString();
     // const thisStatus = status.filter(status => status.id === thisGoal.statusID);
     // const thisHabitType = habitType.filter(type => type.id === thisGoal.habitTypeID);
     // const thisTimePeriod = timePeriod.filter(period => period.id === thisGoal.timePeriodID);
@@ -70,32 +75,28 @@ export default function GoalStats() {
         <Section>
             <div className="select-none border rounded p-4 w-full">
                 <div className="flex flex-row justify-between mb-4 font-bold">
-                    <h3>{goal.goalName}</h3>
+                    <h3>{thisGoal.goalName}</h3>
                     <span>StatusPill</span>
                 </div>
                 <div className="flex flex-row justify-between text-sm mb-3 font-semibold">
                     <span>Habit Name:</span>
-                    <span>{goal.habitName}</span>
+                    <span>{thisGoal.habitName}</span>
                 </div>
                 <div className="flex flex-row justify-between text-sm mb-3">
                     <span>Target:</span>
-                    <span>{goal.dayCount} from {goal.dayCount} Days</span>
+                    <span>{totalHabits} from {totalHabits} Days</span>
                 </div>
                 <div className="flex flex-row justify-between text-sm mb-3">
                     <span>Days Completed:</span>
-                    <span>{goal.daysCompleted} from {goal.dayCount} Days</span>
+                    <span>{completedHabits} from {totalHabits} Days</span>
                 </div>
                 <div className="flex flex-row justify-between text-sm mb-3">
                     <span>Days Failed:</span>
-                    <span>{goal.daysFailed} Days</span>
-                </div>
-                <div className="flex flex-row justify-between text-sm mb-3">
-                    <span>Habit Type:</span>
-                    <span>{goal.habitType}</span>
+                    <span>{totalHabits} Days</span>
                 </div>
                 <div className="flex flex-row justify-between text-sm">
                     <span>Created On:</span>
-                    <span>{createdAtDate.toDateString()}</span>
+                    <span>{createdAtDate}</span>
                 </div>
             </div>
         </Section>
