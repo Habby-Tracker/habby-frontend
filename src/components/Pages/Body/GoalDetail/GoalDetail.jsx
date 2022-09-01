12ca48a5c81985092bb80c5e63b64786255e1970
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-31 17:35:05 -0700

    fix failedHabits and remainingHabits variables in GoalDetail component

--- src/components/Pages/Body/GoalDetail/GoalDetail.jsx
+++ src/components/Pages/Body/GoalDetail/GoalDetail.jsx
@@ -14,8 +14,8 @@ export default function GoalDetail() {
     const goalHabits = (thisGoal && habits && habits.length) ? habits.filter(habit => habit.goalID === Number(thisGoal.id)) : null;
     const totalHabits = goalHabits && goalHabits.length;
     const completedHabits = goalHabits && goalHabits.filter(habit => habit.statusID === '3').length;
-    const failedHabits = goalHabits && goalHabits.filter(habit => habit.statusID !== '3' && new Date(habit.dueDate) < today).length;
-    const remainingHabits = goalHabits && goalHabits.filter(habit => habit.statusID !== '3' && new Date(habit.dueDate) > today).length;
+    const failedHabits = goalHabits && goalHabits.filter(habit => habit.statusID !== '3' && new Date(habit.dueDate).toDateString() < today.toDateString()).length;
+    const remainingHabits = goalHabits && goalHabits.filter(habit => habit.statusID !== '3' && new Date(habit.dueDate).toDateString() >= today.toDateString()).length;
     const createdAtDate = thisGoal && new Date(thisGoal.createdAt).toDateString();
 
     const values = { id, goals, habits, today, thisGoal, goalHabits, totalHabits, completedHabits, failedHabits, remainingHabits, createdAtDate };
